4a32dcad05635b5d71506413c8364118a69d92c9
quanuminh <quan.uminh1@gmail.com>
2026-03-29 22:32:33 +0700

    init project

---
+++ src/main/java/com/fasterthanligh1/ielts_backend/IeltsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fasterthanligh1.ielts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeltsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fasterthanligh1/ielts_backend/IeltsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasterthanligh1.ielts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeltsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
